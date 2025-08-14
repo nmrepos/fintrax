4d5c115f22185af10dcf799df528bc89def38d67
Nidhun Murali <nmurali1611@conestogac.on.ca>
2025-08-14 20:23:59 +0000

    Updated Routes

--- fintrax-app/app/api/[[...route]]/route.ts
+++ fintrax-app/app/api/[[...route]]/route.ts
@@ -1,9 +1,20 @@
 import { Hono } from "hono"
 import { handle } from "hono/vercel";
 
+import summary from "./summary";
+import accounts from "./accounts";
+import categories from "./categories";
+import transactions from "./transactions";
+
 export const runtime = "nodejs";
 
 const app = new Hono().basePath("/api");
+
+const routes = app
+  .route("/summary", summary)
+  .route("/accounts", accounts)
+  .route("/categories", categories)
+  .route("/transactions", transactions)
 
 export const GET = handle(app);
 export const POST = handle(app);
